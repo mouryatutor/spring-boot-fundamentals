a91b75d3cb1df3863943629b7e04ca59e8eb69df
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-03-21 09:13:59 +0000

    Add two numbers

---
+++ Assignment/sumtwonumber/src/main/java/com/ratan/sumtwonumber/Sum.java
@@ -0,0 +1,12 @@
+package com.ratan.sumtwonumber;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sum {
+    
+    public static int SumOfTwoNumber(int num1,int num2)
+    {
+        return num1 + num2;
+    }
+}
