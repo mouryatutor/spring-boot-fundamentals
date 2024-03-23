0d611680bcd21c7f9235a0d99c4b28f9d3175d27
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-23 09:18:28 +0000

    Update and delete , data source

--- productrestapi/src/main/java/com/ratan/springweb/productrestapi/entities/Product.java
+++ productrestapi/src/main/java/com/ratan/springweb/productrestapi/entities/Product.java
@@ -1,8 +1,17 @@
 package com.ratan.springweb.productrestapi.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String description;
     private int price;
