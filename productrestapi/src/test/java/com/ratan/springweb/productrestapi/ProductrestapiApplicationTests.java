78816939e1e362619ee9fd85c2586819161152e8
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-24 15:49:30 +0000

    setProduct and updateProduct testing

--- productrestapi/src/test/java/com/ratan/springweb/productrestapi/ProductrestapiApplicationTests.java
+++ productrestapi/src/test/java/com/ratan/springweb/productrestapi/ProductrestapiApplicationTests.java
@@ -21,4 +21,25 @@ class ProductrestapiApplicationTests {
 		assertEquals("IPhone", product.getName());
 	}
 
+	@Test
+	public void testCreateProduct(){
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = new Product();
+		product.setName("Samsung Mobile");
+		product.setDescription("Its Awesome");
+		product.setPrice(1000);
+		Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products/", product, Product.class);
+		assertNotNull(newProduct);
+		assertNotNull(newProduct.getId());
+		assertEquals("Samsung Mobile", newProduct.getName());
+	}
+
+	@Test
+	public void testUpdateProducts() {
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/2", Product.class);
+		product.setPrice(1400);
+		restTemplate.put("http://localhost:8080/productapi/products/",product);
+	}
+
 }
