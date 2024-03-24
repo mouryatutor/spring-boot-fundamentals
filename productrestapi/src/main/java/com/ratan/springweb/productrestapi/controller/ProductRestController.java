f223ee98a89331a549db8bdb1bd2f07b8fa7fbaf
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-24 17:17:43 +0000

    Logger implemented using slj4j

--- productrestapi/src/main/java/com/ratan/springweb/productrestapi/controller/ProductRestController.java
+++ productrestapi/src/main/java/com/ratan/springweb/productrestapi/controller/ProductRestController.java
@@ -3,6 +3,8 @@ package com.ratan.springweb.productrestapi.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,8 @@ public class ProductRestController {
     @Autowired
     ProductRepository repository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductRestController.class);
+
     @RequestMapping(value = "/products/", method = RequestMethod.GET)
     public List<Product> getProducts() {
         return repository.findAll();
@@ -26,6 +30,7 @@ public class ProductRestController {
 
     @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
     public Product getProduct(@PathVariable("id") int id) {
+        LOGGER.info("Finding product by ID " + id);
         return repository.findById(id).get();
     }
 
