ac60c50799637921a52b2b1b076920f7395ea568
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-24 15:15:29 +0000

    getProduct testing using RestTemplate

--- productrestapi/src/test/java/com/ratan/springweb/productrestapi/ProductrestapiApplicationTests.java
+++ productrestapi/src/test/java/com/ratan/springweb/productrestapi/ProductrestapiApplicationTests.java
@@ -1,13 +1,24 @@
 package com.ratan.springweb.productrestapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import com.ratan.springweb.productrestapi.entities.Product;
 
 @SpringBootTest
 class ProductrestapiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testGetProducts() {
+
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/3", Product.class);
+		assertNotNull(product);
+		assertEquals("IPhone", product.getName());
 	}
 
 }
