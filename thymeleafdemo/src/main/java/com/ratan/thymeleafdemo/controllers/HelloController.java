ed2702b7e7a76bf3e33e873f7c9bb553cbecea88
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-27 18:21:59 +0000

    Sending object to thymleaf

--- thymeleafdemo/src/main/java/com/ratan/thymeleafdemo/controllers/HelloController.java
+++ thymeleafdemo/src/main/java/com/ratan/thymeleafdemo/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ratan.thymeleafdemo.modal.Student;
+
 @Controller
 public class HelloController {
 
@@ -18,6 +20,18 @@ public class HelloController {
         ModelAndView mav = new ModelAndView("data");
         mav.addObject("message", "Take up one idea and make it your life");
         return mav;
+    }
+
+    @RequestMapping("/student")
+    public ModelAndView getStudents()
+    {
+        ModelAndView mav = new ModelAndView("student");
+        Student student = new Student();
+        student.setName("Ratan Mourya");
+        student.setScore(100);
+        mav.addObject("student", student);
+        return mav;
+        
     }
     
 }
---
+++ thymeleafdemo/src/main/java/com/ratan/thymeleafdemo/modal/Student.java
@@ -0,0 +1,24 @@
+package com.ratan.thymeleafdemo.modal;
+
+public class Student {
+    
+    public String name;
+    public Integer score;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getScore() {
+        return score;
+    }
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+
+    
+    
+}
