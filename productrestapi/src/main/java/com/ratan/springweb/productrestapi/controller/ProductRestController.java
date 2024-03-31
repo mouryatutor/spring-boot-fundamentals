92c4aede5528f4218ec800acd49e718c24ecd5eb
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-31 09:22:47 +0000

    caching added in the get product

--- productrestapi/src/main/java/com/ratan/springweb/productrestapi/controller/ProductRestController.java
+++ productrestapi/src/main/java/com/ratan/springweb/productrestapi/controller/ProductRestController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ratan.springweb.productrestapi.entities.Product;
 import com.ratan.springweb.productrestapi.repos.ProductRepository;
+
 
 @RestController
 public class ProductRestController {
@@ -28,7 +32,10 @@ public class ProductRestController {
         return repository.findAll();
     }
 
+
     @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+    @Transactional(readOnly = true)
+    @Cacheable("product-cache")
     public Product getProduct(@PathVariable("id") int id) {
         LOGGER.info("Finding product by ID " + id);
         return repository.findById(id).get();
@@ -46,6 +53,7 @@ public class ProductRestController {
     }
 
     @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    @CacheEvict("product-cache")
     public void deleteProduct(@PathVariable("id") int id) {
         repository.deleteById(id);
     }
--- productrestapi/src/main/java/com/ratan/springweb/productrestapi/entities/Product.java
+++ productrestapi/src/main/java/com/ratan/springweb/productrestapi/entities/Product.java
@@ -1,12 +1,14 @@
 package com.ratan.springweb.productrestapi.entities;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Product {
+public class Product implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
